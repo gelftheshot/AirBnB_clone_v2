0f0aa7a71f2c34f14760caaebacbbcfee6171b96
eliasyeme <eliasyeme@gmail.com>
2023-12-18 16:28:16 +0300

    Add relationship between City and Place models

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,6 +14,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship(
+            "Place", backref="user", cascade="all, delete, delete-orphan"
+        )
     else:
         email = ''
         password = ''
