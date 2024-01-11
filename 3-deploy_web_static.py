fd55bc58ccab04a926311a58b40ecc2bcc3e8d45
eliasyeme <eliasyeme@gmail.com>
2024-01-11 14:14:06 +0300

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,6 +28,7 @@ def do_pack():
     except Exception:
         return None
 
+
 def do_deploy(archive_path):
     """
     Deploys a compressed archive to the web servers
@@ -38,69 +39,27 @@ def do_deploy(archive_path):
     Returns:
         bool: True if the deployment was successful, False otherwise
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        file = archive_path.split("/")[-1]
+        name = file.split(".")[0]
+        put(archive_path, "/tmp/{}".format(file))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file, name))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/*/"
+            "data/web_static/releases/{}/"
+            .format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
+        return True
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
-
-
-# def do_deploy(archive_path):
-#     """
-#     Deploys a compressed archive to the web servers
-
-#     Args:
-#         archive_path (str): The path to the compressed archive
-
-#     Returns:
-#         bool: True if the deployment was successful, False otherwise
-#     """
-#     if not os.path.exists(archive_path):
-#         return False
-#     try:
-#         file = archive_path.split("/")[-1]
-#         name = file.split(".")[0]
-#         put(archive_path, "/tmp/{}".format(file))
-#         run("rm -rf /data/web_static/releases/{}/".format(name))
-#         run("mkdir -p /data/web_static/releases/{}/".format(name))
-#         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-#             .format(file, name))
-#         run("rm /tmp/{}".format(file))
-#         run("mv /data/web_static/releases/{}/web_static/*/"
-#             "data/web_static/releases/{}/"
-#             .format(name, name))
-#         run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-#         run("rm -rf /data/web_static/current")
-#         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-#             .format(name))
-#         return True
-#     except Exception:
-#         return False
 
 
 def deploy():
