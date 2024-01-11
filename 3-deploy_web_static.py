459be3f170f06082cc05dde5a3fc1472186b0b9a
eliasyeme <eliasyeme@gmail.com>
2024-01-11 13:58:21 +0300

    Fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,15 +44,15 @@ def do_deploy(archive_path):
     try:
         file = archive_path.split("/")[-1]
         name = file.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}{}/".format(path, name))
-        run("sudo tar -xzf /tmp/{} -C {}{}/".format(file, path, name))
-        run("sudo rm /tmp/{}".format(file))
-        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, name))
-        run("sudo rm -rf {}{}/web_static".format(path, name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}{}/ /data/web_static/current".format(path, name))
+        put(archive_path, "/tmp/{}".format(file))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/*/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
         return True
     except Exception:
         return False
